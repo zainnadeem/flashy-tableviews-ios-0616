519762bd26127acf3ec9ebc7647f20a99f0cb46b
Joe Burgess <joe@flatironschool.com>
2014-10-21 08:42:24 -0400

    Initial Commit

---
+++ flashy-tableviews/FISAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  FISAppDelegate.h
+//  flashy-tableviews
+//
+//  Created by iOS Staff on 10/21/14
+//  Copyright (c) 2014 The Flatiron School. All rights reserved.
+//
+
+@interface FISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ flashy-tableviews/Resources/Other-Sources/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  flashy-tableviews
+//
+//  Created by iOS Staff on 10/21/14
+//  Copyright (c) 2014 The Flatiron School. All rights reserved.
+//
+
+@import UIKit;
+
+#import "FISAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FISAppDelegate class]));
+    }
+}
