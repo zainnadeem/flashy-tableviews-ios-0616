845b3fa180b4f47e88729f9dc23156d75fa055a0
JimCampagno <jimcampagno@gmail.com>
2015-09-11 10:09:41 -0400

    create xcode project name it FIS-Flashy-Tableviews move folder

---
+++ FIS-Flashy-Tableviews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FIS-Flashy-Tableviews
+//
+//  Created by James Campagno on 9/11/15.
+//  Copyright (c) 2015 James Campagno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FIS-Flashy-TableviewsTests/FIS_Flashy_TableviewsTests.m
@@ -0,0 +1,40 @@
+//
+//  FIS_Flashy_TableviewsTests.m
+//  FIS-Flashy-TableviewsTests
+//
+//  Created by James Campagno on 9/11/15.
+//  Copyright (c) 2015 James Campagno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface FIS_Flashy_TableviewsTests : XCTestCase
+
+@end
+
+@implementation FIS_Flashy_TableviewsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
